c51616be55b8d0457189ee514810c2a5e63e170b
darccau <darccau@protonmail.com>
2021-11-01 05:23:59 -0300

    feat: add beta version

---
+++ sinonimous.py
@@ -0,0 +1,24 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+target_word = sys.argv[1]
+full_url = "https://www.sinonimos.com.br/" + target_word
+
+page = requests.get(full_url)
+
+soup = BeautifulSoup(page.text, "html.parser")
+
+raw_sinon = soup.find_all(class_="s-wrapper")
+
+for index in raw_sinon:
+    sinon = index.text.split(":")
+    sinon = sinon[0] + ":\n" + sinon[1] + "\n"
+    # for i in range(len(sinon)):
+    #    if sinon[i] == ":":
+    #        sinon[i] = "\n"
+    #        break
+    # broken_line = sinon.find(":")
+    # sinon = sinon[broken_line] "\n"
+
+    print(sinon)
