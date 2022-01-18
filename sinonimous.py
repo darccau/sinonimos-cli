6404ff10d569da2c65fb98e659e581a258a858e6
darccau <darccau@protonmail.com>
2022-01-17 21:11:12 -0300

    style: improve text presentation

--- sinonimous.py
+++ sinonimous.py
@@ -11,16 +11,11 @@ soup = BeautifulSoup(page.text, "html.parser")
 
 raw_sinon = soup.find_all(class_="s-wrapper")
 
-print(f"Sinônimo de {target_word}")
+print(f"\n                                    Sinônimo de {target_word}\n")
+
 
 for index in raw_sinon:
     sinon = index.text.split(":")
     sinon = sinon[0] + ":\n" + sinon[1] + "\n"
-    # for i in range(len(sinon)):
-    #    if sinon[i] == ":":
-    #        sinon[i] = "\n"
-    #        break
-    # broken_line = sinon.find(":")
-    # sinon = sinon[broken_line] "\n"
 
     print(sinon)
